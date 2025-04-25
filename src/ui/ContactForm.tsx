b74862faa2dc9bdd53c714b31a07307f8462e540
MariaHajdic <marianneliash@gmail.com>
2025-04-25 22:46:42 +0100

    Test

--- src/ui/ContactForm.tsx
+++ src/ui/ContactForm.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import '../styles/ContactForm.css'
 
-const ContactForm = () => {
+const ContactForm = ({ onClose }: Props) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
